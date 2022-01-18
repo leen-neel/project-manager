f9a7773ff4c0969310105f0dda844b258ffa64e0
leen-neel <and24903@gmail.com>
2022-01-18 19:50:20 +0530

    mongo connect

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require('mongoose')
 require("dotenv").config();
 
 const app = express();
@@ -9,6 +10,16 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+// Connect to DB
+mongoose
+  .connect(process.env.DB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  });
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on http://localhost:${process.env.PORT}`);
